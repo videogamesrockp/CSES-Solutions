4229351c1ca10cbdea2ed44b28b2067ff7b16fd6
Aster Deng <46637392+videogamesrockp@users.noreply.github.com>
2023-10-14 17:21:44 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+// Source: https://usaco.guide/general/io
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
+
+		int n = Integer.parseInt(r.readLine());
+
+		StringTokenizer st = new StringTokenizer(r.readLine());
+		Set<Integer> all_distinct_nums = new HashSet<Integer>();
+
+		for (int i = 0; i < n; i ++) {
+
+			all_distinct_nums.add(Integer.parseInt(st.nextToken()));
+
+		}
+
+		pw.println(all_distinct_nums.size());
+
+		pw.close();
+	}
+}
