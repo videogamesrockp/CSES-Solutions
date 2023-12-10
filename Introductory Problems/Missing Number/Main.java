583aca549cda81ba9cda4ce7fb60a317bd455be1
Aster <aster.deng@gmail.com>
2023-12-09 18:39:17 -0800

    Add Missing Number

--- Introductory Problems/Missing Number/Main.java
+++ Introductory Problems/Missing Number/Main.java
@@ -5,26 +5,26 @@ import java.util.StringTokenizer;
 import java.util.HashSet;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter pw = new PrintWriter(System.out);
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
 
-		int N = Integer.parseInt(r.readLine());
-		HashSet<Integer> nums = new HashSet<Integer>();
+        int N = Integer.parseInt(r.readLine());
+        HashSet<Integer> nums = new HashSet<Integer>();
 
-		StringTokenizer st = new StringTokenizer(r.readLine());
-		
-		for (int i = 0; i < N - 1; i++) {
-			nums.add(Integer.parseInt(st.nextToken()));
-		}
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        
+        for (int i = 0; i < N - 1; i++) {
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
 
-		for (int i = 1; i <= N; i++) {
-			if (!nums.contains(i)) {
-				pw.println(i);
-				break;
-			}
-		}
-		
-		pw.close();
-	}
+        for (int i = 1; i <= N; i++) {
+            if (!nums.contains(i)) {
+                pw.println(i);
+                break;
+            }
+        }
+        
+        pw.close();
+    }
 }
